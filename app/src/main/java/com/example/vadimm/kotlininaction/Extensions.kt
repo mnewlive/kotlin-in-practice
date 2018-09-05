08e2160f0e7883cb1c8cae3ef4b9146d45fc0168
vadimm <vadimm@saltedge.com>
2018-09-05 14:10:40 +0300

    Added Extensions.kt

---
+++ app/src/main/java/com/example/vadimm/kotlininaction/Extensions.kt
@@ -0,0 +1,16 @@
+val String.lastChar: Char
+    get() = get(length - 1)
+
+var StringBuilder.lastChar: Char
+    get() = get(length - 1)
+    set(value) {
+        this.setCharAt(length - 1, value)
+    }
+
+fun main(args:Array<String>) {
+    val someString = "Vadim Krasava"
+    println(someString.lastChar)
+    val anotherString = StringBuilder("Vadim moget?")
+    anotherString.lastChar = '!'
+    println(anotherString)
+}
