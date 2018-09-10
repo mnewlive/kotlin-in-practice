864ba88092f269188c754fcfa5e3f09e0b221cd7
Vadim Morozov <vadimm@saltedge.com>
2018-09-10 23:48:19 +0300

    Added class Random

---
+++ app/src/main/java/com/example/vadimm/kotlininaction/Random.kt
@@ -0,0 +1,23 @@
+package com.example.vadimm.kotlininaction
+
+fun displayStringInJava() {
+    println("12.345-6.A".split("\\.|-".toRegex()))
+}
+
+fun displayStringInKotlin() {
+    println("12.345-6.A".split(".", "-"))
+}
+
+private fun displayManyStrings() {
+    val kotlinLogo = """| //
+                       .|//
+                       .|\\
+                       .| \\"""
+    println(kotlinLogo.trimMargin("."))
+}
+
+fun main(args:Array<String>) {
+    displayStringInJava()
+    displayStringInKotlin()
+    displayManyStrings()
+}
