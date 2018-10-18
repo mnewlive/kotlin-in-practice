a74e246812daecda7c05fbe715aaf5fdfd3ed4af
vadimm <vadimm@saltedge.com>
2018-10-18 13:33:11 +0300

    Create layput for app, comment aapt, added new packages

---
+++ app/src/main/java/com/example/vadimm/kotlininaction/FindAge.kt
@@ -0,0 +1,12 @@
+package com.example.vadimm.kotlininaction
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FindAge : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_age)
+    }
+}
