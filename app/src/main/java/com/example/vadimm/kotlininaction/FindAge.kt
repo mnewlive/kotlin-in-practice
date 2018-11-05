a3c3d92bc9e48901c3b185e3e291de6d136aded8
Vadim Morozov <vadimm@saltedge.com>
2018-11-05 22:52:26 +0200

    Added simple function to display my age

--- app/src/main/java/com/example/vadimm/kotlininaction/FindAge.kt
+++ app/src/main/java/com/example/vadimm/kotlininaction/FindAge.kt
@@ -2,11 +2,20 @@ package com.example.vadimm.kotlininaction
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_find_age.*
 
 class FindAge : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_age)
+
+        button.setOnClickListener {
+            val yearOfBirth: Int = editText.text.toString().toInt()
+            val myAge = 2018 - yearOfBirth
+            ageView.text = "Your Age if $myAge years"
+        }
     }
 }
