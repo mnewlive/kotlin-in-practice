078e45beafec89146a56df2e1d79c6482880c50f
Vadim Morozov <vadimm@saltedge.com>
2018-11-05 23:37:04 +0200

    Modified fun

--- app/src/main/java/com/example/vadimm/kotlininaction/FindAge.kt
+++ app/src/main/java/com/example/vadimm/kotlininaction/FindAge.kt
@@ -2,9 +2,8 @@ package com.example.vadimm.kotlininaction
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_find_age.*
+import java.util.*
 
 class FindAge : AppCompatActivity() {
 
@@ -14,7 +13,8 @@ class FindAge : AppCompatActivity() {
 
         button.setOnClickListener {
             val yearOfBirth: Int = editText.text.toString().toInt()
-            val myAge = 2018 - yearOfBirth
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+            val myAge = currentYear - yearOfBirth
             ageView.text = "Your Age if $myAge years"
         }
     }
