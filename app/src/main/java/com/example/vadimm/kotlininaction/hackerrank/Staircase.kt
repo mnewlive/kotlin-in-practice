1948f7376dac9218339107cefadd31597f03fd8b
vadimm <vadimm@saltedge.com>
2019-04-23 19:12:11 +0300

    Added Staircase

---
+++ app/src/main/java/com/example/vadimm/kotlininaction/hackerrank/Staircase.kt
@@ -0,0 +1,25 @@
+package com.example.vadimm.kotlininaction.hackerrank
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val n = scan.nextLine().trim().toInt()
+    val output = staircase(n)
+    println(output)
+}
+
+fun staircase(n: Int): String {
+    var output = ""
+    for (i in 0 until n) {
+        for (b in 0 until n) {
+            if (b >= (n - i - 1)) {
+                output += "#"
+            } else output += " "
+        }
+        if (i != (n - 1)) {
+            output += "\n"
+        }
+    }
+    return output
+}
