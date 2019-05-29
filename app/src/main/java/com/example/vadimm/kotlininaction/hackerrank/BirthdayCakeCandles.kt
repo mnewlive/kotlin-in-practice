3e1b1551456549c7fa78b6383a68d93c5614d31c
vadimm <vadimm@saltedge.com>
2019-05-29 10:07:22 +0300

    Added Birthday Cake Candles

---
+++ app/src/main/java/com/example/vadimm/kotlininaction/hackerrank/BirthdayCakeCandles.kt
@@ -0,0 +1,19 @@
+package com.example.vadimm.kotlininaction.hackerrank
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    var tallest = 0
+    var frequency = 0
+
+    for (i in 0 until scan.nextInt()) {
+        val height = scan.nextInt()
+
+        if (height > tallest) {
+            tallest = height
+            frequency = 1
+        } else if (height == tallest) frequency++
+    }
+    println(frequency)
+}
