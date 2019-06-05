b456b8ef4ccb913ae81a14e07fdc1334ca13642e
vadimm <vadimm@saltedge.com>
2019-06-05 20:34:11 +0300

    Added Time Conversion

---
+++ app/src/main/java/com/example/vadimm/kotlininaction/hackerrank/TimeConversion.kt
@@ -0,0 +1,24 @@
+package com.example.vadimm.kotlininaction.hackerrank
+
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val time: String = scan.next()
+    val result = timeConversion(time)
+    println(result)
+}
+
+private fun timeConversion(time: String): String {
+    val dateTimeFormat = SimpleDateFormat("hh:mm:ssa")
+    var result = Date()
+    try {
+        result = dateTimeFormat.parse(time)
+    } catch (e: Exception) {
+    }
+    val currentDateOfMarker = SimpleDateFormat("HH:mm:ss", Locale.ENGLISH)
+    return currentDateOfMarker.format(result)
+}
+
