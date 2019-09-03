13c9c40e1b35e96c2b62f688685fc97b4ed1ee17
mnewlive <morozovvadim91@gmail.com>
2019-09-03 19:30:48 +0300

    Remove unused components of code

--- app/src/main/java/com/example/vadimm/kotlininaction/MainActivity.kt
+++ app/src/main/java/com/example/vadimm/kotlininaction/MainActivity.kt
@@ -1,40 +1,12 @@
 package com.example.vadimm.kotlininaction
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Vibrator
-import android.view.View
-import com.example.vadimm.kotlininaction.tools.vibrateOnce
-import kotlinx.android.synthetic.main.activity_main.*
-import android.content.Intent
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private var vibrator: Vibrator? = null
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupCLickListeners()
-        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-    }
-
-    private fun setupCLickListeners() {
-        vibroButton.setOnClickListener(this)
-        findAge.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        val viewId = v?.id ?: return
-        when (viewId) {
-            R.id.vibroButton -> vibrator?.vibrateOnce()
-            R.id.findAge -> findMyAge()
-        }
-    }
-
-    private fun findMyAge() {
-        val intent = Intent(this@MainActivity, FindAge::class.java)
-        startActivity(intent)
     }
 }
