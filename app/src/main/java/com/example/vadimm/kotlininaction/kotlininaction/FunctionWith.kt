0148be1de5922e59615be3b150a8212f1caa4bd6
mnewlive <morozovvadim91@gmail.com>
2019-10-30 21:59:18 +0200

    Tried to use with/apply/buildString

--- app/src/main/java/com/example/vadimm/kotlininaction/kotlininaction/FunctionWith.kt
+++ app/src/main/java/com/example/vadimm/kotlininaction/kotlininaction/FunctionWith.kt
@@ -4,12 +4,40 @@ import java.lang.StringBuilder
 
 fun main(args: Array<String>) {
     println(showAlphabet())
+    println(showAlphabetWithFunctionWith())
+    println(showAlphabetWithFunctionApply())
+    println(showAlphabetWithBuildString())
 }
 
-fun showAlphabet():String = with(StringBuilder()){
-    for(letter in 'A'..'Z') {
+fun showAlphabet(): String {
+    val result = StringBuilder()
+    for (letter in 'A'..'Z') {
+        result.append(letter)
+    }
+    result.append("\n Now I know alphabet")
+    return result.toString()
+}
+
+fun showAlphabetWithFunctionWith() = with(StringBuilder()) {
+    for (letter in 'A'..'Z') {
         append(letter)
     }
-    append("\n Now I know alphabet")
+    append("\n Now I know alphabet with function with")
     toString()
 }
+
+fun showAlphabetWithFunctionApply() =
+    StringBuilder().apply {
+        for (letter in 'A'..'Z') {
+            append(letter)
+        }
+        append("\n Now I know alphabet with function apply")
+    }.toString()
+
+fun showAlphabetWithBuildString() =
+    buildString {
+        for (letter in 'A'..'Z') {
+            append(letter)
+        }
+        append("\n Now I know alphabet with buildString")
+    }
