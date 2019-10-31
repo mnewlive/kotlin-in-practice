9f340625dcf01e4251aef476dacfc41d1e2a1c89
mnewlive <morozovvadim91@gmail.com>
2019-10-31 18:19:54 +0200

    Solved Day6LetsReview

---
+++ app/src/main/java/com/example/vadimm/kotlininaction/hackerrank/thirtyDaysOfCode/Day6LetsReview.kt
@@ -0,0 +1,30 @@
+package com.example.vadimm.kotlininaction.hackerrank.thirtyDaysOfCode
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+
+    scanner.nextLine()
+
+    for (i in 0 until n) {
+        val string = scanner.nextLine()
+        val charArray = string.toCharArray()
+
+        for (j in charArray.indices) {
+            if (j % 2 == 0) {
+                print(charArray[j])
+            }
+        }
+        print(" ")
+
+        for (j in charArray.indices) {
+            if (j % 2 != 0) {
+                print(charArray[j])
+            }
+        }
+        println()
+    }
+    scanner.close()
+}
