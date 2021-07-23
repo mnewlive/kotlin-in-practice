59ea69d349ed69954062df2f9995451d9d98fd40
mnewlive <morozovvadim91@gmail.com>
2021-07-23 18:13:17 +0300

    Finished fourth chapter

--- app/src/main/java/com/example/vadimm/kotlininaction/grokking_algorithms/FourthChapter.kt
+++ app/src/main/java/com/example/vadimm/kotlininaction/grokking_algorithms/FourthChapter.kt
@@ -33,6 +33,7 @@ private fun findMaxNumber(list: IntArray): Int {
     }
 }
 
+// Быстрая сортировка
 private fun quickSort(list: List<Int>): List<Int> {
     //базовый случай, когда массивы уже отсортированы
     if (list.size <= 1) return list
@@ -44,10 +45,30 @@ private fun quickSort(list: List<Int>): List<Int> {
     return quickSort(less) + equal + quickSort(greater)
 }
 
+// Удвоение значения каждого элемента массива.
+private fun doublingElement(list: List<Int>): List<Int> {
+    return list.map { it * 2 }
+}
+
+// Удвоение значения только первого элемента массива.
+private fun doublingFirstElement(list: IntArray): IntArray {
+    val a = list.first() * 2
+    list[0] = a
+    return list
+}
+
+fun doublingFirstElement2(array: IntArray) = array.also { it[0] *= 2 }
+
 fun main() {
     println(sum(arrayOf(2, 4, 6)))
     println(sum2(intArrayOf(2, 4, 7)))
     println(count(listOf(0 ,1, 2, 3, 4, 5)))
     println(findMaxNumber(intArrayOf(1, 2, 3, 15, 10)))
     println(quickSort(listOf(1, 4, 5, 5, 9, 3)))
+    println(doublingElement(listOf(1, 2, 3, 15, 10)))
+
+    println(doublingFirstElement(intArrayOf(1, 2, 3, 15, 10)).contentToString())
+    println(doublingFirstElement2(intArrayOf(1, 2, 3, 15, 10)).joinToString())
+    println(intArrayOf(1, 2, 3, 15, 10).apply { this[0] = this[0] * 2 }.joinToString())
+
 }
